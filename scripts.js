eb0b2e84001a073515289dc9534e0fe84fc74c5f
ria876 <brianna.roper17@gmail.com>
2023-10-28 20:22:37 -0500

    Updated CSS and added JS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-/* Add your JavaScript to this file */
+window.onload=function() {
+    var email = document.getElementsByTagName('input');
+    const error_message = document.getElementsByClassName('message');
+    var submission = document.getElementsByClassName('btn');
+
+    form.addEventListener('submit', function(event) {
+
+        event.preventDefault();
+        if (email.value === '') {
+            error_message.style.display = "Please enter a valid email address";
+        }
+        else {
+            error_message.style.display = "Thank you! Your email address " + email + " has been added to our mailing list!";
+        }
+    }
+    )
+}
+
+
